53bd28404d1d69e9ea9ba73ce4cd7cdbca5f3196
katibu01 <ukmisau@gmail.com>
2023-09-16 12:05:29 +0100

    update

--- resources/views/sales/credit/recent_sales_table.blade.php
+++ resources/views/sales/credit/recent_sales_table.blade.php
@@ -1,4 +1,4 @@
-<div class="card recent-table">      
+<div class="card recent-table">
     <div class="card-body">
         <div class="table-responsive">
             <table class="table recent mb-0">
@@ -12,27 +12,32 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($recents as $key2 => $recent )
-                    @php
-                    $total_amount = 0;
-                        $sales = App\Models\Sale::select('price','quantity','discount')
-                                                ->where('receipt_no', $recent->receipt_no)
-                                                ->get();
-                        foreach ($sales as $sale) {
-                            $total_amount += ($sale->price*$sale->quantity)-$sale->discount;
-                        }
+                    @foreach ($recents as $key2 => $recent)
+                        @php
+                            $total_amount = 0;
+                            $sales = App\Models\Sale::select('price', 'quantity', 'discount')
+                                ->where('receipt_no', $recent->receipt_no)
+                                ->where('business_id', auth()->user()->business_id)
+                                ->where('branch_id', auth()->user()->branch_id)
+                                ->get();
+                            foreach ($sales as $sale) {
+                                $total_amount += $sale->price * $sale->quantity - $sale->discount;
+                            }
                             
-                    @endphp 
+                        @endphp
                         <tr>
                             <td>{{ $key2 + 1 }}</td>
                             <td>{{ $recent->receipt_no }}</td>
                             @php
-                                $name = App\Models\User::select('name')->where('id',$recent->customer_id)->first();
+                                $name = App\Models\User::select('name')
+                                    ->where('id', $recent->customer_id)
+                                    ->first();
                             @endphp
                             <td>{{ @$name->name }}</td>
-                            <td>&#8358;{{ number_format($total_amount,0) }}</td>
+                            <td>&#8358;{{ number_format($total_amount, 0) }}</td>
                             <td>
-                                <button type="button" onclick="PrintReceiptContent('{{ $recent->receipt_no}}')" class="btn btn-secondary btn-sm"><i class="fa fa-print text-white"></i></button>
+                                <button type="button" onclick="PrintReceiptContent('{{ $recent->receipt_no }}')"
+                                    class="btn btn-secondary btn-sm"><i class="fa fa-print text-white"></i></button>
                             </td>
                         </tr>
                     @endforeach
