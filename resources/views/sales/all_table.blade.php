cccd9e1a7c56c8310706aff37830ac127d91fa23
katibu01 <ukmisau@gmail.com>
2023-09-16 11:26:35 +0100

    update

--- resources/views/sales/all_table.blade.php
+++ resources/views/sales/all_table.blade.php
@@ -15,56 +15,69 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($sales as $key => $row )
+            @foreach ($sales as $key => $row)
                 @php
                     $total_amount = 0;
                     $total_discount = 0;
-                    $saled = App\Models\Sale::select('price','quantity','discount','customer_id','created_at','receipt_no','note','collected')
-                                            ->where('receipt_no', $row->receipt_no)
-                                            ->get();
+                    $saled = App\Models\Sale::select('price', 'quantity', 'discount', 'customer_id', 'created_at', 'receipt_no', 'note', 'collected')
+                        ->where('receipt_no', $row->receipt_no)
+                        ->where('business_id', auth()->user()->business_id)
+                        ->where('branch_id', auth()->user()->branch_id)
+                        ->get();
                     foreach ($saled as $sale) {
-                        $total_amount += ($sale->price*$sale->quantity);
-                        $total_discount+= $sale->discount;
+                        $total_amount += $sale->price * $sale->quantity;
+                        $total_discount += $sale->discount;
                     }
-                            
-                @endphp 
-            <tr>
-                @if ($sales instanceof Illuminate\Pagination\LengthAwarePaginator)
-                <td class="text-center">{{ $key + $sales->firstItem() }}</td>
-                 @else
-                 <td class="text-center">{{ $key + 1 }}</td>
-                @endif
-              <th scope="row">{{ $sale->receipt_no }}</th>
-              <td>{{ $saled[0]->created_at->format('l, d F') }}</td>
-              <td>{{ is_numeric($saled[0]->customer_id) ? @$saled[0]->customer->name : @$saled[0]->customer_id}}</td>
-              <td class="text-center">{{ number_format($total_amount,0) }}</td>
-              <td class="text-center">{{ number_format($total_discount,0) }}</td>
-              <td class="text-center">{{ number_format($total_amount-$total_discount,0) }}</td>
-              <td>{{ $saled[0]->note }}</td>
-              <td> @if($saled[0]->collected == 1)<span class="badge bg-success">Collected</span>@else<span class="badge bg-danger">Awaiting</span>@endif</td>
-            <td class="text-center">
-                <div class="dropdown">
-                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="dots"></span>
-                    <span class="dots"></span>
-                    <span class="dots"></span>
-                  </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#" onclick="PrintReceiptContent('{{ $row->receipt_no}}')"><i class="fa fa-print"></i> Receipt</a>
-                    @if($saled[0]->collected == 1)
-                    <a class="dropdown-item" href="#" onclick="confirmPickup('{{ $row->receipt_no}}')">
-                        <i class="fa fa-truck"></i> Awaiting Pickup
-                    </a>
+                    
+                @endphp
+                <tr>
+                    @if ($sales instanceof Illuminate\Pagination\LengthAwarePaginator)
+                        <td class="text-center">{{ $key + $sales->firstItem() }}</td>
                     @else
-                    <a class="dropdown-item" href="#" onclick="confirmDeliver('{{ $row->receipt_no}}')">
-                        <i class="fas fa-shipping-fast"></i> Deliver
-                    </a>
+                        <td class="text-center">{{ $key + 1 }}</td>
                     @endif
-                  </div>
-                </div>
-              </td>
-              
-            </tr>
+                    <th scope="row">{{ $sale->receipt_no }}</th>
+                    <td>{{ $saled[0]->created_at->format('l, d F') }}</td>
+                    <td>{{ is_numeric($saled[0]->customer_id) ? @$saled[0]->customer->name : @$saled[0]->customer_id }}
+                    </td>
+                    <td class="text-center">{{ number_format($total_amount, 0) }}</td>
+                    <td class="text-center">{{ number_format($total_discount, 0) }}</td>
+                    <td class="text-center">{{ number_format($total_amount - $total_discount, 0) }}</td>
+                    <td>{{ $saled[0]->note }}</td>
+                    <td>
+                        @if ($saled[0]->collected == 1)
+                        <span class="badge bg-success">Collected</span>@else<span
+                                class="badge bg-danger">Awaiting</span>
+                        @endif
+                    </td>
+                    <td class="text-center">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="dots"></span>
+                                <span class="dots"></span>
+                                <span class="dots"></span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#"
+                                    onclick="PrintReceiptContent('{{ $row->receipt_no }}')"><i class="fa fa-print"></i>
+                                    Receipt</a>
+                                @if ($saled[0]->collected == 1)
+                                    <a class="dropdown-item" href="#"
+                                        onclick="confirmPickup('{{ $row->receipt_no }}')">
+                                        <i class="fa fa-truck"></i> Awaiting Pickup
+                                    </a>
+                                @else
+                                    <a class="dropdown-item" href="#"
+                                        onclick="confirmDeliver('{{ $row->receipt_no }}')">
+                                        <i class="fas fa-shipping-fast"></i> Deliver
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </td>
+
+                </tr>
             @endforeach
         </tbody>
     </table>
@@ -75,5 +88,4 @@
             @endif
         </ul>
     </nav>
-    </div>
-   
+</div>
--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -372,102 +372,99 @@
 
 
         function PrintReceiptContent(receipt_no) {
-            data = {
-                'receipt_no': receipt_no,
-            }
+    data = {
+        'receipt_no': receipt_no,
+    }
 
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            $.ajax({
-                type: "POST",
-                url: "{{ route('refresh-receipt') }}",
-                data: data,
-                success: function(res) {
-                    var html = '';
-                    var total = 0;
-                    var discount = 0;
-
-    
-
-                    $.each(res.items, function(key, item) {
-                        html +=
-                            '<tr style="text-align: center">' +
-                            '<td style="text-align: left"><span style="font-size: 12px;" >' + item
-                            .product.name + '</span></td>' +
-                            '<td style="font-size: 12px;">' + item.quantity + '</td>' +
-                            '<td style="font-size: 12px;">' + item.price.toLocaleString() + '</td>' +
-                            '<td style="font-size: 12px;">' + (item.quantity * item.price)
-                            .toLocaleString() + '</td>' +
-                            '</tr>';
-                        total += item.quantity * item.price;
-                        discount += item.discount;
-                    });
-
-                    var totalCal = total;
-
-                    $('#receipt_body').html(html);
-                    $('.tran_id').html('S' + res.items[0].receipt_no);
-                    $('#cashier_name').html(res.staff);
-
-                    if (discount > 0) {
-                        $('#salesdiscount').html('₦' + discount.toLocaleString());
-                        $("#salesdiscounttr").show();
-                        totalCal = total - discount;
-                    }else{
-                        $("#salesdiscounttr").hide();
-                    }
-                    $('#total').html('₦' + totalCal.toLocaleString());
-
-                    var data = document.getElementById('print').innerHTML;
-
-                    myReceipt = window.open("", "myWin", "left=150, top=130,width=300, height=400");
-
-                    myReceipt.screenX = 0;
-                    myReceipt.screenY = 0;
-                    myReceipt.document.write(data);
-                    myReceipt.document.title = "Print Receipt";
-                    myReceipt.focus();
-                    myReceipt.print();
-                },
-
-                error: function(xhr, ajaxOptions, thrownError) {
-                    if (xhr.status === 419) {
-                        Command: toastr["error"](
-                            "Session expired. please login again."
-                        );
-                        toastr.options = {
-                            closeButton: false,
-                            debug: false,
-                            newestOnTop: false,
-                            progressBar: false,
-                            positionClass: "toast-top-right",
-                            preventDuplicates: false,
-                            onclick: null,
-                            showDuration: "300",
-                            hideDuration: "1000",
-                            timeOut: "5000",
-                            extendedTimeOut: "1000",
-                            showEasing: "swing",
-                            hideEasing: "linear",
-                            showMethod: "fadeIn",
-                            hideMethod: "fadeOut",
-                        };
-                        setTimeout(() => {
-                            window.location.replace('{{ route('login') }}');
-                        }, 2000);
-                    }
-                },
-            });
-
-
-            setTimeout(() => {
-                // myReceipt.close();
-            }, 8000);
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "{{ route('refresh-receipt') }}",
+        data: data,
+        success: function(res) {
+            var html = '';
+            var total = 0;
+            var discount = 0;
+
+            $.each(res.items, function(key, item) {
+                html +=
+                    '<tr style="text-align: center">' +
+                    '<td style="text-align: left"><span style="font-size: 12px;" >' + item
+                    .product.name + '</span></td>' +
+                    '<td style="font-size: 12px;">' + item.quantity + '</td>' +
+                    '<td style="font-size: 12px;">' + item.price.toLocaleString() + '</td>' +
+                    '<td style="font-size: 12px;">' + (item.quantity * item.price)
+                    .toLocaleString() + '</td>' +
+                    '</tr>';
+                total += item.quantity * item.price;
+                discount += item.discount;
+            });
+
+            var totalCal = total;
+
+            $('#receipt_body').html(html);
+            $('.tran_id').html('S' + res.items[0].receipt_no);
+            $('#cashier_name').html(res.staff);
+
+            if (discount > 0) {
+                $('#salesdiscount').html('₦' + discount.toLocaleString());
+                $("#salesdiscounttr").show();
+                totalCal = total - discount;
+            } else {
+                $("#salesdiscounttr").hide();
+            }
+            $('#total').html('₦' + totalCal.toLocaleString());
+
+            var data = document.getElementById('print').innerHTML;
+
+            myReceipt = window.open("", "myWin", "left=150, top=130,width=300, height=400");
+
+            myReceipt.screenX = 0;
+            myReceipt.screenY = 0;
+            myReceipt.document.write(data);
+            myReceipt.document.title = "Print Receipt";
+            myReceipt.focus();
+            myReceipt.print();
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            if (xhr.status === 419) {
+                Command: toastr["error"](
+                    "Session expired. please login again."
+                );
+                toastr.options = {
+                    closeButton: false,
+                    debug: false,
+                    newestOnTop: false,
+                    progressBar: false,
+                    positionClass: "toast-top-right",
+                    preventDuplicates: false,
+                    onclick: null,
+                    showDuration: "300",
+                    hideDuration: "1000",
+                    timeOut: "5000",
+                    extendedTimeOut: "1000",
+                    showEasing: "swing",
+                    hideEasing: "linear",
+                    showMethod: "fadeIn",
+                    hideMethod: "fadeOut",
+                };
+                setTimeout(() => {
+                    window.location.replace('{{ route('login') }}');
+                }, 2000);
+            }
+        },
+    });
+
+    setTimeout(() => {
+        // myReceipt.close();
+    }, 8000);
+}
+
     </script>
 
     <script>
