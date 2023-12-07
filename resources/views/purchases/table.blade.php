510d2107e7cd54559a15bd4d5527aba6788b178d
katibu01 <ukmisau@gmail.com>
2023-12-07 22:35:35 +0100

    update meta

--- resources/views/purchases/table.blade.php
+++ resources/views/purchases/table.blade.php
@@ -29,31 +29,6 @@
                     </td>
                 </tr>
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal{{ $key }}" tabindex="-1" role="dialog"
-                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Delete ?</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form action="{{ route('stock.delete') }}" method="post">
-                                    @csrf
-                                    <p>You cannot undo this operation once executed.</p>
-                                    <input type="hidden" name="id" value="{{ @$purchase->id }}">
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-danger ml-2">Delete</button>
-                            </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
             @endforeach
 
         </tbody>
